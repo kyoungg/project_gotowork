d2f6cc2a7f8964e16c80b6fc2427e2ef8ff2d823
kyunghye <kyoung00828@gmail.com>
2025-03-06 04:24:01 +0900

    feat: 작업관리자 ui 구현

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,87 @@
 import styled from "styled-components";
 
+import { MdOutlineBroadcastOnHome } from "react-icons/md";
+import { AiOutlineCloudSync } from "react-icons/ai";
+import { IoIosArrowUp } from "react-icons/io";
+import { AiOutlineSound } from "react-icons/ai";
+
 const Footer = () => {
-  return <Background></Background>;
+  return (
+    <Background>
+      <StartBtn>
+        <Img src="./public/windowslogo.png" />
+        시작
+      </StartBtn>
+      <MenuContainer>
+        <IoIosArrowUp size="28" />
+        <MdOutlineBroadcastOnHome size="28" />
+        <AiOutlineCloudSync size="28" />
+        <AiOutlineSound size="28" />
+        <Clock>
+          <p>오후 5:13</p>
+          <p>2025-03-05</p>
+        </Clock>
+      </MenuContainer>
+    </Background>
+  );
 };
 
 export default Footer;
 
 const Background = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
   background-color: #c2c5b0;
 
   width: 1024px;
   height: 60px;
+`;
+
+const StartBtn = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: #c2c5b0;
+  border: solid #9e9d90 2px;
+  box-shadow: 1px 1px 1px 1px gray;
+
+  width: 85px;
+  height: 35px;
+  margin-left: 15px;
+
+  font-size: 24px;
+`;
+
+const MenuContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  background-color: #c2c5b0;
+  border: solid #9e9d90 2px;
+  box-shadow: 1px 1px 2px 2px gray inset;
+
+  width: 231px;
+  height: 35px;
+  margin-right: 15px;
+`;
+
+const Img = styled.img`
+  height: 30px;
+  width: 30px;
+`;
+
+const Clock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+
+  width: 80px;
+  height: 30px;
+  padding-left: 10px;
+
+  font-size: 16px;
 `;
